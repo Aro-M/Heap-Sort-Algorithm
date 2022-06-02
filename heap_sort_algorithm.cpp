0f9c3bab254705fd592a8a6cb1ca8b6eb43973bd
Aro MIlitosyan <ar.militosyan.97@mail.ru>
2022-06-02 18:16:02 +0400

    Update heap_sort_algorithm.cpp
    Heap Sort  Algorithm with recursive

--- heap_sort_algorithm.cpp
+++ heap_sort_algorithm.cpp
@@ -1,50 +1,43 @@
-#include <iostream>
-#include <utility>
+#include  <iostream>
 
-void heapify(int arr[], int number, int i)
-{
-	int biggest = i; 
-	int left = 2 * i + 1; 
-	int right = 2 * i + 2; 
-
-	if (left < number && arr[left] > arr[biggest])
-		biggest = left;
-
-	if (right < number && arr[right] > arr[biggest])
-		biggest = right;
-
-	if (biggest != i) {
-		std::swap(arr[i], arr[biggest]);
-
-		heapify(arr, number, biggest);
-	}
+void heapify(int arr_heap[], int number , int i){
+    
+    int biggest = i;
+    int left  = 2*i+1;
+    int right = 2*i+2;
+	
+    if( left < number && arr_heap[left] > arr_heap[biggest]){
+        biggest = left;
+    }
+    if(right < number && arr_heap[right] > arr_heap[biggest]){
+        biggest = right;
+    }
+    if(biggest != i ){
+        std::swap(arr_heap[i] , arr_heap[biggest]);
+        heapify(arr_heap,number ,biggest);
+    }   
 }
 
-void heap_sort(int arr[], int number)
-{
-	for (int i = number / 2 - 1; i >= 0; i--)
-		heapify(arr, number, i);
-
-	for (int i = number - 1; i >= 0; i--) {
-		std::swap(arr[0], arr[i]);
-
-		heapify(arr, i, 0);
-	}
+void print_heap_sort(int arr_heap[], int  number){
+    for(int i = 0; i < number; i++){
+        std::cout << arr_heap[i] <<  " ";
+    }
+}
+void  heap_sort(int arr_heap[],int number){
+    for(int i = number/2-1; i >= 0 ; --i){
+        heapify(arr_heap ,  number , i );  
+    }
+    for(int i = number-1; i >= 0; --i){
+        std::swap(arr_heap[i] ,arr_heap[0]);  
+        heapify(arr_heap, i , 0);
+    }
 }
 
-void print_array(int arr[], int n)
-{
-	for (int i = 0; i < n; ++i)
-	    std::cout << arr[i] << " ";
-
+int main(){
+    
+    int arr_heap[] = {1,8,2,5,2,14,8,299,211,892,41,5,3,852,52};
+    int number  =  sizeof(arr_heap) /sizeof(arr_heap[0]);
+    heap_sort(arr_heap, number);
+    std::cout << "Heap sort element is  " ; 
+    print_heap_sort(arr_heap,number);
 }
-
-int main()
-{
-	int arr[] = { 12, 11, 13,8,77,854,18,78,5,2,368,1,12,1,21,3,9,33,9,92,5,77,4,23,9,28,6, 5, 6, 7 };
-	int number = sizeof(arr) / sizeof(arr[0]);
-	heap_sort(arr, number);
-	std::cout << "Heap sort  element is "<<std::endl;
-	print_array(arr, number);
-}
-
